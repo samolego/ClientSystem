edd8d1aa1957c0978cf522bc523ab33fa9a7bfb5
samo_lego <34912839+samolego@users.noreply.github.com>
2020-09-17 21:37:39 +0200

    Template files

---
+++ src/main/java/org/samo_lego/clientsystem/ClientSystem.java
@@ -0,0 +1,10 @@
+package org.samo_lego.clientsystem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientSystem implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// huh, what now?
+	}
+}
